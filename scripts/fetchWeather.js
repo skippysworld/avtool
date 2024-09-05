cb810679f31e7fcb1e196057c1c1e60be95e0c4e
Skippy <92035101+skippysworld@users.noreply.github.com>
2024-09-05 20:52:04 +0200

    Latest changes, API Broken

--- scripts/fetchWeather.js
+++ scripts/fetchWeather.js
@@ -1,11 +1,19 @@
 const renderedMETAR = document.querySelector("#rendered-metar");
 const renderedTAF = document.querySelector("#rendered-taf");
 
-let station = "lkpr";
-
-const fetchMETAR = await fetch(
-	`https://aviationweather.gov/api/data/metar?ids=${station}&format=json`
-);
+function fetchMETAR(station = "lkpr") {
+	fetch(`https://aviationweather.gov/api/data/metar?ids=lkpr&format=json`, {
+		method: "post",
+		mode: "no-cors",
+	})
+		.then((response) => console.log(response)) //.json())
+		.then((data) => {
+			console.log(data);
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+}
 
 // function fetchTAF(station = "lkpr") {
 // 	fetch(`https://aviationweather.gov/api/data/taf?ids=${station}&format=json`)
