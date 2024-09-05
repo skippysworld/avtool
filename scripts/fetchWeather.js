b0ef15413e0a6cb0a0429dd971b2e3112619a7bc
Skippy <92035101+skippysworld@users.noreply.github.com>
2024-09-05 20:13:49 +0200

    API Update

--- scripts/fetchWeather.js
+++ scripts/fetchWeather.js
@@ -1,27 +1,22 @@
 const renderedMETAR = document.querySelector("#rendered-metar");
 const renderedTAF = document.querySelector("#rendered-taf");
 
-function fetchMETAR(station = "lkpr") {
-	fetch(`https://aviationweather.gov/api/data/metar?ids=${station}&format=json`)
-		.then((response) => response.json())
-		.then((data) => {
-			console.log(data);
-		})
-		.catch((error) => {
-			console.log(error);
-		});
-}
+let station = "lkpr";
 
-function fetchTAF(station = "lkpr") {
-	fetch(`https://aviationweather.gov/api/data/taf?ids=${station}&format=json`)
-		.then((response) => response.json())
-		.then((data) => {
-			console.log(data);
-		})
-		.catch((error) => {
-			console.log(error);
-		});
-}
+const fetchMETAR = await fetch(
+	`https://aviationweather.gov/api/data/metar?ids=${station}&format=json`
+);
+
+// function fetchTAF(station = "lkpr") {
+// 	fetch(`https://aviationweather.gov/api/data/taf?ids=${station}&format=json`)
+// 		.then((response) => response.json())
+// 		.then((data) => {
+// 			console.log(data);
+// 		})
+// 		.catch((error) => {
+// 			console.log(error);
+// 		});
+// }
 
 fetchMETAR();
-fetchTAF();
+// fetchTAF();
